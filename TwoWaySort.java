48c741abe6e346ac9671ed2af33488fbd8e40718
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 19:40:21 +0100

    Add TwoWaySort

---
+++ TwoWaySort.java
@@ -0,0 +1,50 @@
+public class TwoWaySort {
+
+    static void eosort(int[] t) {
+
+        printInts(t);
+
+        int i, ei, oi, l = t.length;
+
+        for (i=ei=0; i < l; i++)
+            if (t[i] % 2 == 0) ei++;
+
+        int[] r = new int[l],
+              e = new int[ei],
+              o = new int[l-ei];
+
+        for (i=ei=oi=0; i < l; i++) {
+            if (t[i] % 2 == 0) e[ei++] = t[i];
+            else o[oi++] = t[i];
+        }
+
+        java.util.Arrays.sort(e);
+        java.util.Arrays.sort(o);
+        for(i = 0, oi=o.length; i < oi/2; i++) {
+            int s = o[i];
+            o[i] = o[oi-i-1];
+            o[oi-i-1] = s;
+        }
+
+        for (i=ei=oi=0; i < l; i++)
+            r[i] = t[i] % 2 == 0 ? e[ei++] : o[oi++];
+
+        printInts(r);
+
+    }
+
+    static void printInts(int[] t) {
+        for (int i = 0; i < t.length; i++)
+            System.out.print(t[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] t = {0,1,2,3,4,5,6,7,8,9};
+
+        eosort(t);
+
+    }
+
+}

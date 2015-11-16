d0b89f18b0f3b8e12ca72251701adc8e0878d48b
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 19:40:32 +0100

    Add TwoWaySort.js

---
+++ TwoWaySort.js
@@ -0,0 +1,19 @@
+function eosort(t) {
+
+    printInts(t);
+
+    var b = [t.filter(x => x % 2 == 0).sort(),
+             t.filter(x => x % 2).sort().reverse()],
+        i = [0,0], r = t.map(x => b[x%2][i[x%2]++]);
+
+    printInts(r);
+
+}
+
+function printInts(t) {
+    console.log(t.join(' '));
+}
+
+var t = [0,1,2,3,4,5,6,7,8,9];
+
+eosort(t);

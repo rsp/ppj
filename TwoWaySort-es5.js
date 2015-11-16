ebd166d589509b691ae1c4518c9601997c0950a6
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 21:39:32 +0100

    Add TwoWaySort-es5.js

---
+++ TwoWaySort-es5.js
@@ -0,0 +1,19 @@
+function eosort(t) {
+
+    var b = [t.filter(x => x % 2 == 0).sort(),
+             t.filter(x => x % 2).sort().reverse()],
+        i = [0,0], r = t.map(function(x){ return b[x%2][i[x%2]++]; });
+
+    return r;
+
+}
+
+function printInts(t) {
+    console.log(t.join(' '));
+}
+
+var t = [0,1,2,3,4,5,6,7,8,9];
+
+printInts(t);
+
+printInts(eosort(t));

d5f3458f874ace5520b1b13656486c8fff645634
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 19:17:43 +0100

    Update WhereEven.scm

--- WhereEven.scm
+++ WhereEven.scm
@@ -1,6 +1,8 @@
-(define (even t) (filter even? t))
+(define (even t)
+    (filter even? t))
 
-(define (print-ints t) (display t) (newline))
+(define (print-ints t)
+    (display t) (newline))
 
 (define t (list 0 1 2 3 4 5 6 7 8 9))
 (print-ints t)

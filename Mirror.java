4346775c80d1896e0d9a0ca2fabb888c867fb23a
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 18:58:41 +0100

    Add Mirror.java

---
+++ Mirror.java
@@ -0,0 +1,20 @@
+public class Mirror {
+
+    static String mirror(String s) {
+        return mirror(s, "");
+    }
+
+    static String mirror(String s, String m) {
+        return s.length() == 0 ? m :
+            mirror(s.substring(1), s.substring(0,1) + m);
+    }
+
+    public static void main(String[] args) {
+
+        String s = "to be or not to be";
+
+        System.out.format("%s -> %s\n", s, mirror(s));
+
+    }
+
+}
